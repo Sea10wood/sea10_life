063b8c0aac7f20ee61e3265385b7e21abfc4e34e
Sea10wood <miyusea5@gmail.com>
2024-12-29 11:47:30 +0900

    add: ステッカー、吹き出しアニメーション

--- app/components/FloatingImage.tsx
+++ app/components/FloatingImage.tsx
@@ -8,11 +8,17 @@ export default function FloatingImage() {
         <Image
           src="/sea10.webp"
           alt="Sea10"
+          width={500} 
+          height={500}
           className="w-[300px] sm:w-[400px] md:w-[500px] drop-shadow-lg"
           priority
         />
-        <div className="absolute top-[-40px] left-1/2 transform -translate-x-1/2 bg-white text-black px-4 py-2 rounded-lg shadow-lg">
-          <p className="text-center text-sm sm:text-base whitespace-nowrap">こんにちは！Sea10です！</p>
+        <div className={`absolute top-[-40px] left-1/2 transform ${styles.floatBubble}`}>
+          <div className="bg-white text-black px-4 py-2 rounded-lg shadow-lg">
+            <p className="text-center text-sm sm:text-base whitespace-nowrap">
+              こんにちは！Sea10です！
+            </p>
+          </div>
           <div className="absolute bottom-[-10px] left-1/2 transform -translate-x-1/2 w-0 h-0 border-8 border-transparent border-t-white"></div>
         </div>
       </div>
